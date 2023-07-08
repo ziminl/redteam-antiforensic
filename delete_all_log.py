94d00c0330639ef27c6f781c211227d067eafe2b
crossrepublic <71635944+ziminl@users.noreply.github.com>
2023-07-08 12:27:16 +0900

    Create delete_all_log.py

---
+++ delete_all_log.py
@@ -0,0 +1,17 @@
+
+
+
+import os
+
+def delete_log_files(directory):
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith(".log"):
+                file_path = os.path.join(root, file)
+                print(f"Deleting file: {file_path}")
+                os.remove(file_path)
+
+directory_to_search = "C:/path/to/directory"
+delete_log_files(directory_to_search)
+
+
